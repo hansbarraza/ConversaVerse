40fada1dc8a039faacad15acb75347d8f4bede46
Hans Barraza <brain@Hanss-MacBook-Pro.local>
2023-08-23 21:26:59 -0700

    added navbar with a sign in button with modals also linked it to google authenticaion for sign in and connected to planetscale database

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,42 @@
-import '@/styles/globals.css'
+import NavBar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toaster";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
-  title: 'ConversaVerse',
-  description: 'A Reddit clone like built with Next.js and TypeScript',
-}
+  title: "ConversaVerse",
+  description: "A Reddit clone like built with Next.js and TypeScript",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        {/* @ts-expect-error server component */}
+        <NavBar />
+
+        {authModal}
+
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+        <Toaster />
+      </body>
     </html>
-  )
+  );
 }
